80c1cd6f6af1b8e8b82f854e15c71af15f344975
Taylor <36935794+taybug@users.noreply.github.com>
2018-03-07 15:42:22 -0600

    Update printDOM.js

--- printDOM.js
+++ printDOM.js
@@ -1,1 +1,14 @@
 // copy working script code here
+
+// create a loop to "print out" all elements in the dom.
+// 1. createa my loop structure
+// 2. how so i access an element on my page and  then all of them.
+// 3. how do i print them all out. - start on the console
+var all = index.html;
+
+function printDom(){
+for (var i=0, max=all.length; i < max; i++) {
+ // Do something with the element here
+console.log(all);
+}
+   }
