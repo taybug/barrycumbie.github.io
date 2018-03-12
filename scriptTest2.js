5501888ed335eada97ed656d156492869c6e2290
Taylor <36935794+taybug@users.noreply.github.com>
2018-03-12 16:17:45 -0500

    Add files via upload

---
+++ scriptTest2.js
@@ -0,0 +1,27 @@
+// create a loop to "print out" all elements in the dom.
+// 1. createa my loop structure
+// 2. how so i access an element on my page and  then all of them.
+//.getElementByTagName('*');
+//console.log(all);
+
+console.log('A'); 
+
+function printDom(){
+var all = document;
+console.log('B'); 
+for (var i=0, max=all.length; i < max; i++) {
+	
+	console.log(all[i]);
+}
+
+if (document.getElementsByTagName)
+    var elements = document.getElementsByTagName(all);
+else
+    // loop through all elements instead
+print(elements);
+  };
+
+console.log('C'); 
+
+printDom(); 
+// 3. how do i print them all out. - start on the console
